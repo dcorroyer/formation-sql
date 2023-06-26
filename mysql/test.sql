a1c1beb02ca106e7c85c8d54927f82524854d9cd
dcorroyer <dylan.corroyer@wanadoo.fr>
2023-06-26 21:26:10 +0200

    insert and update in sql

--- mysql/test.sql
+++ mysql/test.sql
@@ -72,3 +72,62 @@ SELECT *
 FROM recipes
 WHERE slug = 'soupe'
   AND duration < 30;
+
+SELECT *
+FROM recipes
+WHERE slug = 'soupe'
+  OR duration > 20;
+
+SELECT *
+FROM recipes
+WHERE slug IN ('soupe');
+
+SELECT *
+FROM recipes
+WHERE (slug = 'soupe' OR duration > 20) AND online = true;
+
+SELECT *
+FROM recipes
+WHERE slug LIKE 'salade%';
+
+INSERT INTO recipes
+(title,
+ slug,
+ content,
+ duration,
+ online,
+ created_at)
+VALUES ('Quiche',
+        'quiche',
+        'Contenu de test',
+        40,
+        FALSE,
+        NOW()),
+       ('Lasagnes',
+        'lasagne',
+        'Contenu de test',
+        60,
+        TRUE,
+        NOW());
+
+DELETE FROM recipes WHERE title = 'Salade de fruit';
+
+SELECT * FROM recipes;
+
+INSERT INTO recipes
+(title,
+ slug,
+ content,
+ duration,
+ online,
+ created_at)
+VALUES ('Salade de fruit',
+        'salade-de-fruit',
+        'Contenu de test',
+        30,
+        TRUE,
+        NOW());
+
+UPDATE recipes SET title = 'Soupe de légumes' WHERE title = 'Soupe';
+
+UPDATE recipes SET title = 'Soupe de fleurs', slug = 'soupe-de-fleurs' WHERE id = 2;
